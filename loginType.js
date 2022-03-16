49e43f755bd0a69fcfabb361d0a52296a61f3158
Pranjal Sharma <ps9395@gmail.com>
2022-03-16 15:15:14 +0530

    lastest style

--- loginType.js
+++ loginType.js
@@ -2,8 +2,8 @@ const googleLogin=document.querySelector('.google')
 const faceLogin=document.querySelector('.facebook')
 
 googleLogin.addEventListener('click',(e)=>{
-  window.location.href="https://share--x.herokuapp.com/public/google"
+  window.location.href="http://https://share--x.herokuapp.com/public/google"
 })
 faceLogin.addEventListener('click',(e)=>{
-  window.location.href="https://share--x.herokuapp.com/public/facebook"
+  window.location.href="http://https://share--x.herokuapp.com/localhost:5000/public/facebook"
 })
