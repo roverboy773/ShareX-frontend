0a870027e1145026f4396838c209172d28dbba7f
pranjal <ps9395@gmail.com>
2022-04-04 12:03:42 +0530

    latest

---
+++ public/loginEmail.js
@@ -0,0 +1,24 @@
+document.getElementById("submit").addEventListener('click',async(e)=>{
+    e.preventDefault()
+
+    let email=document.getElementById("email").value
+    let password=document.getElementById("password").value
+    
+    let data={email,password}
+    
+    
+    const result =await fetch('http://localhost:5000/login',{
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body:JSON.stringify(data),
+      })
+      const res=await result.json();
+      console.log(res)
+    if(res.user_id!==undefined){
+      localStorage.setItem('@Auth',JSON.stringify({user:res.user_id,name:res.name}))
+      window.location.href="/public/index.html"
+    }
+})
